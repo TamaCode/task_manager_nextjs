6519cf732a7d12e9de9e4c4b15de23d8edda80dd
Martin Tamareu <mg.tamareu@gmail.com>
2022-11-06 01:43:05 -0300

    Implemento el listado de tasks en el componente Home y agrego sus estilos

--- src/context/taskContext.js
+++ src/context/taskContext.js
@@ -4,7 +4,7 @@ import { v4 as uuid } from "uuid";
 export const TaskContext = createContext();
 
 export const TasksProvider = ({ children }) => {
-  const [tasks, setTasks] = useState([{}, {}, {}]);
+  const [tasks, setTasks] = useState([]);
 
   const createTask = ({ title, description }) => {
     setTasks([...tasks, { title, description, id: uuid() }]);
--- src/pages/index.js
+++ src/pages/index.js
@@ -3,10 +3,31 @@ import { useContext } from "react";
 import Layout from "../components/Layout";
 
 const Home = () => {
-  const {} = useContext(TaskContext);
+  const { tasks } = useContext(TaskContext);
+
   return (
     <Layout>
-      <div className="m-10">Hello World</div>
+      <div>
+        {tasks.length === 0 ? (
+          <h2>There are no Tasks</h2>
+        ) : (
+          <div className="w-7/12">
+            {tasks.map((task, index) => (
+              <div
+                key={index}
+                className="bg-gray-700 hover:bg-gray-600 cursor-pointer px-20 py-5 m-2 flex justify-between"
+              >
+                <span>{index}</span>
+                <div>
+                  <h1 className="font-bold">{task.title}</h1>
+                  <p className="text-gray-300">{task.description}</p>
+                  <span className="text-gray-400">{task.id}</span>
+                </div>
+              </div>
+            ))}
+          </div>
+        )}
+      </div>
     </Layout>
   );
 };
