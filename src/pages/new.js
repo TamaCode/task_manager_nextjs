5f9b596802cf452bbca46c2c926d14419c404da5
Martin Tamareu <mg.tamareu@gmail.com>
2022-11-07 22:56:45 -0300

    Le doy estilos a las cards del form de tareas

--- src/pages/new.js
+++ src/pages/new.js
@@ -34,31 +34,33 @@ const TaskFormPage = () => {
 
   return (
     <Layout>
-      <form onSubmit={handleSubmit}>
-        <h1>{query.id ? "Update Task" : "Create Task"}</h1>
-        <input
-          className="bg-gray-800 focus:text-gray-100 focus:outline-none w-full py-3 px-4 mb-5"
-          type="text"
-          name="title"
-          placeholder="Write a title"
-          onChange={handleChange}
-          value={task.title}
-        />
-        <textarea
-          className="bg-gray-800 focus:text-gray-100 focus:outline-none w-full py-3 px-4 mb-5"
-          rows="2"
-          name="description"
-          placeholder="Write a description"
-          onChange={handleChange}
-          value={task.description}
-        ></textarea>
-        <button
-          className="bg-green-500 hover:bg-green-400 px-4 py-2 rounded-md disabled:opacity-30"
-          disabled={!task.title || !task.description}
-        >
-          Save
-        </button>
-      </form>
+      <div className="flex justify-center items-center h-full">
+        <form onSubmit={handleSubmit} className="bg-gray-700 p-10 h-2/4">
+          <h1 className="text-3xl mb-7">{query.id ? "Update Task" : "Create Task"}</h1>
+          <input
+            className="bg-gray-800 focus:text-gray-100 focus:outline-none w-full py-3 px-4 mb-5"
+            type="text"
+            name="title"
+            placeholder="Write a title"
+            onChange={handleChange}
+            value={task.title}
+          />
+          <textarea
+            className="bg-gray-800 focus:text-gray-100 focus:outline-none w-full py-3 px-4 mb-5"
+            rows="2"
+            name="description"
+            placeholder="Write a description"
+            onChange={handleChange}
+            value={task.description}
+          ></textarea>
+          <button
+            className="bg-green-500 hover:bg-green-400 px-4 py-2 rounded-md disabled:opacity-30"
+            disabled={!task.title || !task.description}
+          >
+            Save
+          </button>
+        </form>
+      </div>
     </Layout>
   );
 };
